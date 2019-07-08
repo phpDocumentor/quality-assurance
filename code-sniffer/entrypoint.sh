55eca3d9d3fe84fa4caac6582bbb70a4fbabe408
Jaapio <jaap@ijaap.nl>
2019-07-08 20:07:25 +0200

    Setup automated builds for github actions

--- code-sniffer/entrypoint.sh
+++ code-sniffer/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
-if [[ "$1" == 'phpcbf' ]]; then
+if [ "$1" = 'phpcbf' ]; then
     sh -c "/root/.composer/vendor/bin/phpcbf ${*:7}"
 else
     sh -c "/root/.composer/vendor/bin/phpcs $*"
