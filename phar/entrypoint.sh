a70be3514e86697295d3ffdb00e8fd79b4511b8e
Jaapio <jaap@ijaap.nl>
2019-07-28 20:14:55 +0200

    Add phar box

---
+++ phar/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -l
+
+
+if [ "$1" = 'box' ]; then
+    sh -c "/root/.composer/vendor/bin/box ${*:3}"
+elif [ "$1" = 'gpg' ]; then
+    echo "$SECRET_KEY" > keys.asc
+    echo "prepare import keys"
+    gpg --batch --yes --import keys.asc
+    echo $PASSPHRASE | gpg ${*:3}
+elif [ "$1" = 'php' ]; then
+    sh -c "php ${*:3}"
+fi
