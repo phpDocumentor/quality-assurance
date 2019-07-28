26fc0578a1e9afe5bb8e637fb5ab7efcc7667950
Jaapio <jaap@ijaap.nl>
2019-07-28 21:01:10 +0200

    Fixes for posix sh

--- code-sniffer/entrypoint.sh
+++ code-sniffer/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 if [ "$1" = 'phpcbf' ]; then
-    sh -c "/root/.composer/vendor/bin/phpcbf ${*:7}"
+    COMMAND=$(echo "${*}" | cut -c 8-)
+    sh -c "/root/.composer/vendor/bin/phpcbf ${COMMAND}"
 else
     sh -c "/root/.composer/vendor/bin/phpcs $*"
 fi
--- phar/entrypoint.sh
+++ phar/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -l
 
-
+COMMAND=$(echo "${*}" | cut -c 5-)
 if [ "$1" = 'box' ]; then
-    sh -c "/root/.composer/vendor/bin/box ${*:3}"
+    sh -c "/root/.composer/vendor/bin/box ${COMMAND}"
 elif [ "$1" = 'gpg' ]; then
     echo "$SECRET_KEY" > keys.asc
     echo "prepare import keys"
     gpg --batch --yes --import keys.asc
-    echo "$PASSPHRASE" | gpg "${*:3}"
+    sh -c "echo $PASSPHRASE | gpg ${COMMAND}"
 elif [ "$1" = 'php' ]; then
-    sh -c "php ${*:3}"
+    sh -c "php ${COMMAND}"
 fi
