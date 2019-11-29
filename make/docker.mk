09c44907773140a80d766d1b88bfa3f8839a0486
Jaapio <jaap@ijaap.nl>
2019-11-29 16:20:13 +0100

    Fix docker tagging

--- make/docker.mk
+++ make/docker.mk
@@ -10,7 +10,7 @@ docker-build: ## Build the top level Dockerfile using the directory or $IMAGE_NA
 
 .PHONY: docker-tag
 docker-tag: ## Tag the docker image using the tag script.
-	tag $(IMAGE_NAME) $(DOCKER_REPO)/$(IMAGE_NAME) --no-latest
+	docker tag $(IMAGE_NAME) $(DOCKER_REPO)/$(IMAGE_NAME)
 
 .PHONY: docker-publish
 docker-publish: docker-tag ## Publish the image and tags to a repository.
