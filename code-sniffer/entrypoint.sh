c51cf9e76ce2ec5e2e4c228ba0c60159f71a4425
Jaapio <jaap@ijaap.nl>
2020-02-05 21:22:52 +0100

    move cs & phpstan to separate repo

--- code-sniffer/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -l
-if [ "$1" = 'phpcbf' ]; then
-    COMMAND=$(echo "${*}" | cut -c 8-)
-    sh -c "/root/.composer/vendor/bin/phpcbf ${COMMAND}"
-else
-    sh -c "/root/.composer/vendor/bin/phpcs $*"
-fi
